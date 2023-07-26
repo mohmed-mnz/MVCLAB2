ce34309423a111412c5c237d863cb59183adeab7
mo7med mnz <mohmedmnz74@gmail.com>
2023-07-26 17:48:29 +0300

    Add project files.

---
+++ MVCLAB2/Controllers/InstructorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLAB2.Models;
+using MVCLAB2.viewmodel;
+
+namespace MVCLAB2.Controllers
+{
+    public class InstructorController : Controller
+    {
+        DB_ITI_Context db;
+        public InstructorController()
+        {
+            db = new DB_ITI_Context();
+        }
+        public IActionResult Index()
+        {
+            var ins=db.Instructors.ToList();
+            return View(ins);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Instructor ins =db.Instructors.SingleOrDefault(x => x.Id == id);
+            Department Dept=db.Departments.SingleOrDefault(y => y.Id ==ins.DeptId);
+            Course crs= db.Courses.SingleOrDefault(c => c.Id == ins.CourseId);
+            InsDeptCrs MD=new InsDeptCrs();
+            MD.Ins_Id = ins.Id;
+            MD.Ins_Name = ins.Name;
+            MD.Ins_Image = ins.image;
+            MD.Ins_Address = ins.Address;
+            MD.Ins_Salary = ins.Salary;
+            MD.Ins_Department = Dept.Name;
+            MD.Ins_Course = crs.Name;
+            return View(MD);
+        }
+    }
+}
---
+++ MVCLAB2/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCLAB2.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Degree { get; set; }
+        public int MinDegree { get; set; }
+        public virtual List<CrsResult>? CourseResults { get; set; }
+        [ForeignKey(nameof(Department))]
+        public int? DepartmentId { get; set; }
+        public virtual Department? Department { get; set; }
+        public virtual List<Instructor>? Instructors { get; set; }
+    }
+}
